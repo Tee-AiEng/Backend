ff5d9bc6f46e8162c8d5dc31a70be2dbbfeef541
Nababa Tee <nababatee@gmail.com>
2025-10-16 13:57:34 +0100

    cfffxddd

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from dotenv import load_dotenv
+import uvicorn
+load_dotenv()
+import os
+
+app = FastAPI(title="Simple FastAPI", version="1.0.0")
+
+@app.get("/",description="This endpoint returns a welcome message")
+def root():
+    return {"Message": "Welcome to my FastAPI Application"}
+
+
+if __name__ == "__main__":
+    print(os.getenv("host"))
+    print(os.getenv("port"))
+    uvicorn.run(app, host=os.getenv("host"), port=int(os.getenv("port")))
 
